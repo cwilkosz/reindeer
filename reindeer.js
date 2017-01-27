40bee95f3b67ecee9e50d81ed30fd165ded66582
C Wilkosz <theewilkyway@gmail.com>
2017-01-26 20:18:00 -0600

    Add files via upload
    Reindeer Games

---
+++ reindeer.js
@@ -0,0 +1,17 @@
+//var fruit = ["cherries", "apples", "bananas"];
+//var fruitString = fruit.join(" cats");
+//console.log("fruitString", fruitString);
+//var colors = ["Blue", "Red", "Orange", "Purple", "Hazel", "Aquamarine", "Periwinkle", "Azure", "Fuchsia", "Chocolate", "Amber", "Amaranth"];
+//var reindeer = ["Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"];
+
+
+var reindeerColors = ["Blue", "Red", "Orange", "Purple", "Hazel", "Aquamarine", "Periwinkle", "Azure", "Fuchsia", "Chocolate", "Amber", "Amaranth"];
+var reindeerColorsString = reindeerColors.join(" Reindeer");
+console.log("reindeerColorsString", reindeerColorsString);
+
+var reindeerNames = ["Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"];
+var reindeerNamesString = reindeerNames.join(" Reindeer");
+console.log("reindeerNamesString", reindeerNamesString);
+
+
+var hohohoElement = document.getElementById("coloredReindeer");
